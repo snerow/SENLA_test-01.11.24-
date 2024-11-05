38ec368305b9d8865e7f0f1e85eaea4cac1f0da0
snerow <148005736+snerow@users.noreply.github.com>
2024-11-06 00:05:46 +0300

    Добавление проверки для Выбора валют
    Повторюсь - надеюсь, ещё можно обновлять прогу.. 
    Лишь недавно понял, что именно подразумевается в Задании №2

--- Задание 2. Курс валют/ExchangeRate.java
+++ Задание 2. Курс валют/ExchangeRate.java
@@ -5,17 +5,30 @@ public class ExchangeRate {
 
     public void convert() {
         Scanner sc = new Scanner(System.in);
+        boolean isRightInput = false;
+        short selectedCurrency = 0;     // Эта переменная не будет использоваться в программе со значением 0.
+                                        // Поставил 0, ибо IntelliJ IDEA ругается
 
-        System.out.println("Выберите валюту, которую будете конвертировать:");
-        System.out.println("1 - Рубли;");
-        System.out.println("2 - Доллар США;");
-        System.out.println("3 - Евро;");
-        System.out.println("4 - Китайский юань;");
-        System.out.println("5 - Казахстанский тенге;");
-        System.out.println("6 - Турецких лир.");
-        System.out.print("Выбор валюты: ");
-        short selectedCurrency = sc.nextShort();
-        sc.nextLine();
+        while (!isRightInput) {
+            System.out.println("Выберите валюту, которую будете конвертировать:");
+            System.out.println("1 - Рубли;");
+            System.out.println("2 - Доллар США;");
+            System.out.println("3 - Евро;");
+            System.out.println("4 - Китайский юань;");
+            System.out.println("5 - Казахстанский тенге;");
+            System.out.println("6 - Турецких лир.");
+            System.out.print("Выбор валюты: ");
+            selectedCurrency = sc.nextShort();
+            sc.nextLine();
+
+            if (selectedCurrency >= 1 && selectedCurrency <= 6) {
+                isRightInput = true;
+            }
+            else {
+                System.out.println("Неправильный ввод. " +
+                                    "Пожалуйста, введите в категорие Выбор валюты значение от 1 до 6.\n");
+            }
+        }
 
         System.out.println("\nВведите сумму конвертации:\t");
         double money = sc.nextDouble();
@@ -29,23 +42,23 @@ public class ExchangeRate {
                 break;
             case 2:
                 ConversionOfUSD conUSD = new ConversionOfUSD(money);
-                conUSD.convert();;
+                conUSD.convert();
                 break;
             case 3:
                 ConversionOfEUR conEUR = new ConversionOfEUR(money);
-                conEUR.convert();;
+                conEUR.convert();
                 break;
             case 4:
                 ConversionOfCNY conCNY = new ConversionOfCNY(money);
-                conCNY.convert();;
+                conCNY.convert();
                 break;
             case 5:
                 ConversionOfKZT conKZT = new ConversionOfKZT(money);
-                conKZT.convert();;
+                conKZT.convert();
                 break;
             case 6:
                 ConversionOfTRY conTRY = new ConversionOfTRY(money);
-                conTRY.convert();;
+                conTRY.convert();
                 break;
         }
 
